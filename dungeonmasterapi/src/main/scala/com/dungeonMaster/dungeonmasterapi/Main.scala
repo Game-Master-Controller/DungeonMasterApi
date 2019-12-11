8424d97dd3fd02f109a56866a91998ad623529a7
Shane Drafahl <sdrafahl@pop-os.localdomain>
2019-12-10 20:52:06 -0600

    Initial commit

---
+++ dungeonmasterapi/src/main/scala/com/dungeonMaster/dungeonmasterapi/Main.scala
@@ -0,0 +1,9 @@
+package com.dungeonMaster.dungeonmasterapi
+
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
+
+object Main extends IOApp {
+  def run(args: List[String]) =
+    DungeonmasterapiServer.stream[IO].compile.drain.as(ExitCode.Success)
+}
